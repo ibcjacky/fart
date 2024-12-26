507596423f25dce1a51db39b5f7dfe4dac319a33
Faded <40565810+ibcjacky@users.noreply.github.com>
2024-12-26 19:42:11 +0800

    fx

---
+++ fart/demo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from fart import box_text
+from fart import FONT_NAMES
+from fart import fart, sample_farts
+
+# sample_farts("automation tool")
+# fart("automation_tool", font_name="small_slant")
+fart("automation_tool", font_name="stick_letters")
+# res = fart("demo line", oneline=True, copy=False)
+# print("+++" + res)
+
+
+# print(box_text("No \\n in text"))
+# print(FONT_NAMES)
